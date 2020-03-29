8f2e8764ace1efbecbf0b613230ce922cb164c60
Lingchao Xin <douglarek@gmail.com>
2020-03-29 23:54:39 +0800

    Panic test improved

--- mechanism/panic/panic.go
+++ mechanism/panic/panic.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 // 演示无法在主 goroutine 里 revocer 一个子 goroutine 的 panic
 func testPanic() {
@@ -15,13 +12,18 @@ func testPanic() {
 
 	}()
 
+	c := make(chan struct{})
+
 	go func() {
+		defer func() {
+			c <- struct{}{}
+		}()
 		panic("I do panic") // 无法被外部 defer recover
 	}()
 
+	<-c
 }
 
 func main() {
 	testPanic()
-	time.Sleep(1 * time.Second)
 }
--- mechanism/panic1/panic.go
+++ mechanism/panic1/panic.go
@@ -2,28 +2,33 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"runtime"
 )
 
 // 无法 defer recover map 并发写的 panic
 func testPanic() {
 	m := make(map[int]struct{})
 
-	for i := 0; i < 5; i++ {
+	c := make(chan struct{}, runtime.NumCPU()*2) // 使用 2 倍 cpu 数更好体现并发写
+
+	for i := 0; i < cap(c); i++ {
 		go func(i int) {
 			defer func() {
 				if e := recover(); e != nil {
 					fmt.Println("panic catched") // 不会走这里
 				}
+				c <- struct{}{}
 			}()
 
 			m[i] = struct{}{}
 		}(i)
 	}
 
+	for i := 0; i < cap(c); i++ {
+		<-c
+	}
 }
 
 func main() {
 	testPanic()
-	time.Sleep(1 * time.Second)
 }
